72f4e90e9d9a157b5bb82c1cd966c066dd7ae8a8
SarvarKhujaev <uchixaitachi6@gmail.com>
2024-03-17 20:23:18 +0500

    изменено:      ../../../../../main/java/com/hibernate/hibernateapplication/HibernateApplication.java

--- src/main/java/com/hibernate/hibernateapplication/HibernateApplication.java
+++ src/main/java/com/hibernate/hibernateapplication/HibernateApplication.java
@@ -1,32 +1,13 @@
 package com.hibernate.hibernateapplication;
 
-import junit.extensions.RepeatedTest;
-import junit.framework.JUnit4TestAdapter;
-
-import org.junit.runner.JUnitCore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.hibernate.hibernateapplication.testing.JavaTest;
-import com.hibernate.hibernateapplication.inspectors.LogInspector;
 import com.hibernate.hibernateapplication.database.HibernateConnector;
 
 @SpringBootApplication
 public class HibernateApplication {
     public static void main( final String[] args ) {
-        /*
-        запускаем тесты
-        */
-        new LogInspector(
-                new JUnitCore().run(
-                        new RepeatedTest(
-                                new JUnit4TestAdapter( JavaTest.class ), 3
-                        )
-                )
-        );
-
-        HibernateConnector.getInstance().close();
-
         SpringApplication.run( HibernateApplication.class, args );
+        HibernateConnector.getInstance().close();
     }
 }
